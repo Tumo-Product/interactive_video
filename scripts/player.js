1b32c6253ed8e4c4e61ec71cfe11413b90418304
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-07-26 10:33:20 +0400

    removal of unnecesary comments

--- scripts/player.js
+++ scripts/player.js
@@ -3,7 +3,6 @@ const player = {
         play : () => {
             document.getElementById(`v_${current_video}`).play();
             view.change_styles(0);
-            // view.hide_question();
         },
     
         pause : () => {
@@ -16,7 +15,6 @@ const player = {
 
             if (currentVideo.currentTime == currentVideo.duration && time < 0)
             {
-                // view.hide_question();
                 player.controls.play();
             }
 
