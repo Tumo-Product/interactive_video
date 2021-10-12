b7226cb4d6d12568f71c6f43b0e9175f62be7d9c
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-10-12 18:19:40 +0400

    local videos for now

--- scripts/parser.js
+++ scripts/parser.js
@@ -13,8 +13,8 @@ const vids = {
     segments: [
         {
             id      : "0",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q1.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q1.mp4",
+            src     : "videos/Q1.mp4",
+            loopSrc : "videos/Loop Q1.mp4",
             choices : [
                 {
                     name : "Answer 1",
@@ -29,8 +29,8 @@ const vids = {
 
         {
             id      : "00",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/A1 + Q2.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q2.mp4",
+            src     : "videos/A1 + Q2.mp4",
+            loopSrc : "videos/Loop Q2.mp4",
             choices : [
                 {
                     name : "Answer 1",
@@ -44,8 +44,8 @@ const vids = {
         },
         {
             id      : "01",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/A2 + Q2.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q2.mp4",
+            src     : "videos/A2 + Q2.mp4",
+            loopSrc : "videos/Loop Q2.mp4",
             choices : [
                 {
                     name : "Answer 1",
@@ -60,8 +60,8 @@ const vids = {
 
         {
             id      : "000",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q2A1 + Q3.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q3.mp4",
+            src     : "videos/Q2A1 + Q3.mp4",
+            loopSrc : "videos/Loop Q3.mp4",
             choices : [
                 {
                     name : "option 01",
@@ -75,8 +75,8 @@ const vids = {
         },
         {
             id      : "001",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q2A2 + Q3.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q3.mp4",
+            src     : "videos/Q2A2 + Q3.mp4",
+            loopSrc : "videos/Loop Q3.mp4",
             choices : [
                 {
                     name : "option 01",
@@ -91,8 +91,8 @@ const vids = {
 
         {
             id      : "0000",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q3A1 + Q4.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q4.mp4",
+            src     : "videos/Q3A1 + Q4.mp4",
+            loopSrc : "videos/Loop Q4.mp4",
             choices : [
                 {
                     name : "option 01",
@@ -106,8 +106,8 @@ const vids = {
         },
         {
             id      : "0001",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q3A2 + Q4.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q4.mp4",
+            src     : "videos/Q3A2 + Q4.mp4",
+            loopSrc : "videos/Loop Q4.mp4",
             choices : [
                 {
                     name : "option 01",
@@ -122,8 +122,8 @@ const vids = {
 
         {
             id      : "00000",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q4A1 + Q5.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q5.mp4",
+            src     : "videos/Q4A1 + Q5.mp4",
+            loopSrc : "videos/Loop Q5.mp4",
             choices : [
                 {
                     name : "option 01",
@@ -137,8 +137,8 @@ const vids = {
         },
         {
             id      : "00001",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q4A2 + Q5.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q5.mp4",
+            src     : "videos/Q4A2 + Q5.mp4",
+            loopSrc : "videos/Loop Q5.mp4",
             choices : [
                 {
                     name : "option 01",
@@ -153,8 +153,8 @@ const vids = {
 
         {
             id      : "000000",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q5A1 + Q6.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q6.mp4",
+            src     : "videos/Q5A1 + Q6.mp4",
+            loopSrc : "videos/Loop Q6.mp4",
             choices : [
                 {
                     name : "option 01",
@@ -168,8 +168,8 @@ const vids = {
         },
         {
             id      : "000001",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q5A2 + Q6.mp4",
-            loopSrc : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Loop Q6.mp4",
+            src     : "videos/Q5A2 + Q6.mp4",
+            loopSrc : "videos/Loop Q6.mp4",
             choices : [
                 {
                     name : "option 01",
@@ -184,12 +184,12 @@ const vids = {
 
         {
             id      : "0000000",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q6A1 + VO.mp4",
+            src     : "videos/Q6A1 + VO.mp4",
             choices : []
         },
         {
             id      : "0000001",
-            src     : "http://40.74.57.201/VideoStorage/videos/CanYouHelpMe/Q6A2 + VO.mp4",
+            src     : "videos/Q6A2 + VO.mp4",
             choices : []
         },
     ]
