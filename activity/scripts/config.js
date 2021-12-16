8a4d46d077f1457ae70bf179c578fdff0d96d65c
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-12-16 11:53:57 +0400

    server changed

--- activity/scripts/config.js
+++ activity/scripts/config.js
@@ -1,4 +1,4 @@
-axios.defaults.baseURL = "https://blackboxbasic.herokuapp.com/";
+axios.defaults.baseURL = "https://content-tools.tumo.world:4000/";
 
 const config = {
     query_url: "interactive_video/getsetsone?_uid="
